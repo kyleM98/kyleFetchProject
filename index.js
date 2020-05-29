3eb29564a00cb0ac609124fd9f20e37fb60d8f73
kyle martin <kylemartin98@outlook.com>
2020-05-29 15:44:10 +0100

    project

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-function addToPage(){
-fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall', {
-  method: 'POST',
-  body: JSON.stringify({message: prompt("Add Here:")})
-}).then(function (response) {
-  if (response.status == 200) {
-    console.log("Ok!")
-  } else {console.log("Oh no!")}
-})
-
-fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall').then(response => response.json()).then(data => console.log(data))
-
-if (localStorage.getItem('test') == "key") {
-  console.log("It's there!")
-} else {
-  localStorage.setItem('test', 'key')
-
+function addToWall() {
+    let theInput = document.getElementById("fname").value
+    fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall', {
+      method: 'POST',
+      body: JSON.stringify({
+        message: theInput
+      })
+    }).then(function(response) {
+      if (response.status == 200) {
+        console.log("Ok!")
+      } else {
+        console.log("Oh no!")
+      }
+    })
 }
-}
+  
+  fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall').then(response => response.json()).then(data => addElements(data))
+  
+  if (localStorage.getItem('test') == "key") {
+    console.log("It's there!")
+  } else {
+    localStorage.setItem('test', 'key')
+  }
+  
+  function addElements(list) {
+    list.forEach(function(element) {
+      let newLi = document.createElement("li")
+      let newContent = document.createTextNode(element)
+      newLi.appendChild(newContent)
+      let currentUl = document.getElementById("list")
+      document.body.insertBefore(newLi, currentUl)
+    })
+  }
