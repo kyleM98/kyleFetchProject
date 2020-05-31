75d68fd2966c0506ec76657e652ca4ae6ab0a238
kyle martin <kylemartin98@outlook.com>
2020-05-31 21:39:49 +0100

    project

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ function addToPage() {
       body: JSON.stringify({
         message: theInput
       })
-    })/*.then(function(response) {
+    }).then(function(response) {
       if (response.status == 200) {
         console.log("Ok!")
       } else {
         console.log("Oh no!")
       }
     })
-}*/
+}
   
   fetch('https://2ubiyjczwh.execute-api.eu-west-2.amazonaws.com/wall').then(response => response.json()).then(data => addElements(data))
   
-  /*if (localStorage.getItem('test') == "key") {
+  if (localStorage.getItem('test') == "key") {
     console.log("It's there!")
   } else {
     localStorage.setItem('test', 'key')
-  }*/
+  }
   
   function addElements(list) {
     list.forEach(function(element) {
